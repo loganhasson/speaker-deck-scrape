3627d4b4d84a19a02070e5f4e8e4bf965b47ed3a
Logan Hasson <logan.hasson@gmail.com>
2013-10-07 22:18:36 -0400

    Scrape all details

--- lib/deck.rb
+++ lib/deck.rb
@@ -15,42 +15,42 @@ class Deck
   end
   
   def scrape
-    # scrape_title
-    # scrape_author
-    # scrape_date
-    # scrape_stars
-    # scrape_views
+    scrape_title
+    scrape_author
+    scrape_date
+    scrape_stars
+    scrape_views
     scrape_category
   end
 
-  # def scrape_title
-  #   @title = self.noko_doc.css('#talk-details h1').text.strip.split(' ').join(' ')
-  #   puts "Title: " + self.title
-  # end
+  def scrape_title
+    @title = self.noko_doc.css('#talk-details h1').text.strip.split(' ').join(' ')
+    puts "Title: " + self.title
+  end
 
-  # def scrape_author
-  #   @author = self.noko_doc.css('#talk-details h2 a').text.strip
-  #   puts "Author: " + self.author
-  # end
+  def scrape_author
+    @author = self.noko_doc.css('#talk-details h2 a').text.strip
+    puts "Author: " + self.author
+  end
 
-  # def scrape_date
-  #   @date = self.noko_doc.css('#talk-details p mark').text[/.*\d{4}/].strip
-  #   puts Chronic.parse(self.date)
-  # end
+  def scrape_date
+    @date = self.noko_doc.css('#talk-details p mark').text[/.*\d{4}/].strip
+    puts "Date: " + Chronic.parse(self.date).to_s
+  end
 
-  # def scrape_stars
-  #   @stars = self.noko_doc.css('.stargazers').children.first.text.scan(/\d/).join.to_i
-  #   puts "Stars: " + self.stars.to_s
-  # end
+  def scrape_stars
+    @stars = self.noko_doc.css('.stargazers').children.first.text.scan(/\d/).join.to_i
+    puts "Stars: " + self.stars.to_s
+  end
 
-  # def scrape_views
-  #   @views = self.noko_doc.css('.views span').text.scan(/\d/).join.to_i
-  #   puts "Views: " + self.views.to_s
-  # end
+  def scrape_views
+    @views = self.noko_doc.css('.views span').text.scan(/\d/).join.to_i
+    puts "Views: " + self.views.to_s
+  end
 
   def scrape_category
-    @category = self.noko_doc.css('.talk-details p mark a')
-    puts self.category
+    @category = self.noko_doc.css('#talk-details p mark a').text.strip
+    puts "Category: " + self.category
   end
 
 end
